60a92be6727aba57c8bb05a49618fd78085f2b02
hachimaki-dev <hachimakidev@gmail.com>
2026-09-21 16:36:19 -0300

    fundamentos sql

---
+++ 004D/1 - Taller_Spotify.sql
@@ -0,0 +1,42 @@
+DROP TABLE CANCION;
+
+--Esto esta en 1FN
+CREATE TABLE CANCION(
+    id_cancion NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    titulo VARCHAR2(200),
+    artista VARCHAR2(200),
+    album VARCHAR2(200),
+    genero VARCHAR2(100),
+    duracion_segundos NUMBER,
+    fecha_lanzamiento DATE
+);
+
+INSERT INTO CANCION(titulo, artista, album, genero, duracion_segundos, fecha_lanzamiento) VALUES ('No me hables de sufrir', 'Los bunkers', 'La culpa', 'Rock latino', 200, DATE '2003-09-01');
+
+INSERT INTO CANCION(titulo, artista, album, genero, duracion_segundos, fecha_lanzamiento) VALUES ('La exiliada del sur', 'Los bunkers', 'La culpa', 'Rock latino', 240, DATE '2003-09-01');
+
+INSERT INTO CANCION(titulo, artista, album, genero, duracion_segundos, fecha_lanzamiento) VALUES ('Mientele', 'Los bunkers', 'Vida de perros', 'Rock latino', 186, DATE '2005-09-08');
+
+COMMIT;
+
+
+CREATE TABLE ARTISTA(
+    id_artista NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    nombre VARCHAR2(100)
+);
+
+INSERT INTO ARTISTA(nombre) VALUES('Los Bunkers');
+INSERT INTO ARTISTA(nombre) VALUES('Pink Floyd');
+INSERT INTO ARTISTA(nombre) VALUES('Eric Clapton');
+INSERT INTO ARTISTA(nombre) VALUES('BB King');
+INSERT INTO ARTISTA(nombre) VALUES('David Gilmoure');
+INSERT INTO ARTISTA(nombre) VALUES('Slash');
+INSERT INTO ARTISTA(nombre) VALUES('Guns and Roses');
+INSERT INTO ARTISTA(nombre) VALUES('Iron Maiden');
+INSERT INTO ARTISTA(nombre) VALUES('Bruno Mars');
+INSERT INTO ARTISTA(nombre) VALUES('Bad Bunny');
+INSERT INTO ARTISTA(nombre) VALUES('Kid Voodo');
+INSERT INTO ARTISTA(nombre) VALUES('31 minutos');
+INSERT INTO ARTISTA(nombre) VALUES('BKN');
+INSERT INTO ARTISTA(nombre) VALUES('Los Pulentos');
+COMMIT;
