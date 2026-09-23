a1f607b2a37dc8aea3a589fb03df7f3d3b31291d
hachimaki-dev <hachimakidev@gmail.com>
2026-09-23 19:44:59 -0300

    avances

--- 001V/1 - Taller_spotify.sql
+++ 001V/1 - Taller_spotify.sql
@@ -10,6 +10,14 @@ CREATE TABLE CANCION(
     fecha_lanzamiento DATE 
 );
 
+
+CREATE TABLE ARTISTA(
+    id_artista NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    nombre VARCHAR2(100),
+    nacionalidad VARCHAR(100),
+    esta_verificado CHAR(1)
+);
+
 INSERT INTO CANCION( titulo, artista, album, genero, duracion_segundos, fecha_lanzamiento) VALUES ( 'Black', 'Pearl Jam', 'Ten', 'Grunch', 180, DATE '1991-08-17');
 
 INSERT INTO CANCION( titulo, artista, album, genero, duracion_segundos, fecha_lanzamiento) VALUES ( 'Jeremy', 'Pearl Jam', 'Ten', 'Grunch', 230, DATE '1991-08-17');
@@ -17,3 +25,19 @@ INSERT INTO CANCION( titulo, artista, album, genero, duracion_segundos, fecha_la
 INSERT INTO CANCION( titulo, artista, album, genero, duracion_segundos, fecha_lanzamiento) VALUES ( 'Alive', 'PEARLJAM', 'Ten', 'Grunch', 450, DATE '1991-08-17');
 
 COMMIT;
+
+SELECT * FROM CANCION;
+
+
+
+INSERT INTO ARTISTA(nombre, nacionalidad, esta_verificado) VALUES('Los Prisioneros', 'Chilena', 'S');
+INSERT INTO ARTISTA(nombre, nacionalidad, esta_verificado) VALUES('Nirvana', 'EE.UU', 'S');
+INSERT INTO ARTISTA(nombre, nacionalidad, esta_verificado) VALUES('Queen', 'Britanicos', 'S');
+INSERT INTO ARTISTA(nombre, nacionalidad, esta_verificado) VALUES('PinkPanters', 'Britanicos', 'N');
+INSERT INTO ARTISTA(nombre, nacionalidad, esta_verificado) VALUES('Michael Jakson', 'EE.UU', 'S');
+INSERT INTO ARTISTA(nombre, nacionalidad, esta_verificado) VALUES('Pearl Jam', 'EE.UU', 'S');
+
+COMMIT;
+
+
+
