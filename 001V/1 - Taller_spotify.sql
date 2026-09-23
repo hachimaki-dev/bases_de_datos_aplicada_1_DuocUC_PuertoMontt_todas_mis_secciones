7084a80f5e5ec4058911d8fe34db22d7e1ab56fb
hachimaki-dev <hachimakidev@gmail.com>
2026-09-23 20:18:27 -0300

    avances

--- 001V/1 - Taller_spotify.sql
+++ 001V/1 - Taller_spotify.sql
@@ -3,7 +3,7 @@ DROP TABLE CANCION;
 CREATE TABLE CANCION(
     id_cancion NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     titulo VARCHAR2(200),
-    artista VARCHAR2(100),
+    id_artista NUMBER REFERENCES ARTISTA(id_artista),
     album VARCHAR2(200),
     genero VARCHAR2(100),
     duracion_segundos NUMBER,
@@ -18,17 +18,24 @@ CREATE TABLE ARTISTA(
     esta_verificado CHAR(1)
 );
 
-INSERT INTO CANCION( titulo, artista, album, genero, duracion_segundos, fecha_lanzamiento) VALUES ( 'Black', 'Pearl Jam', 'Ten', 'Grunch', 180, DATE '1991-08-17');
+INSERT INTO CANCION( titulo, id_artista, album, genero, duracion_segundos, fecha_lanzamiento) VALUES ( 'Black', 6, 'Ten', 'Grunch', 180, DATE '1991-08-17');
 
-INSERT INTO CANCION( titulo, artista, album, genero, duracion_segundos, fecha_lanzamiento) VALUES ( 'Jeremy', 'Pearl Jam', 'Ten', 'Grunch', 230, DATE '1991-08-17');
+INSERT INTO CANCION( titulo, id_artista, album, genero, duracion_segundos, fecha_lanzamiento) VALUES ( 'Jeremy', 6, 'Ten', 'Grunch', 230, DATE '1991-08-17');
 
-INSERT INTO CANCION( titulo, artista, album, genero, duracion_segundos, fecha_lanzamiento) VALUES ( 'Alive', 'PEARLJAM', 'Ten', 'Grunch', 450, DATE '1991-08-17');
+INSERT INTO CANCION( titulo, id_artista, album, genero, duracion_segundos, fecha_lanzamiento) VALUES ( 'Alive', 6, 'Ten', 'Grunch', 450, DATE '1991-08-17');
+
+
+INSERT INTO CANCION(titulo, id_artista, album, genero, duracion_segundos, fecha_lanzamiento) VALUES ('La voz de los 80', 1, 'La voz de los 80', 'Rock', 324, DATE '1984-12-13');
+
+INSERT INTO CANCION(titulo, id_artista, album, genero, duracion_segundos, fecha_lanzamiento) VALUES ('Para amar', 1, 'La voz de los 80', 'Rock', 400, DATE '1984-12-13');
+
+INSERT INTO CANCION(titulo, id_artista, album, genero, duracion_segundos, fecha_lanzamiento) VALUES ('Chicago', 5, 'Xscape', 'Pop', 458, DATE '2014-05-12');
+
+INSERT INTO CANCION(titulo, id_artista, album, genero, duracion_segundos, fecha_lanzamiento) VALUES ('Slave to the rythm', 5, 'Xscape', 'Pop', 254, DATE '2014-05-12');
 
 COMMIT;
 
 SELECT * FROM CANCION;
-
-
 
 INSERT INTO ARTISTA(nombre, nacionalidad, esta_verificado) VALUES('Los Prisioneros', 'Chilena', 'S');
 INSERT INTO ARTISTA(nombre, nacionalidad, esta_verificado) VALUES('Nirvana', 'EE.UU', 'S');
@@ -40,4 +47,13 @@ INSERT INTO ARTISTA(nombre, nacionalidad, esta_verificado) VALUES('Pearl Jam', '
 COMMIT;
 
 
+SELECT * FROM CANCION;
+SELECT * FROM ARTISTA;
 
+
+-- JOINS/ INTERSECCIONES
+SELECT * FROM CANCION JOIN ARTISTA ON CANCION.ID_ARTISTA = ARTISTA.ID_ARTISTA;
+
+SELECT * FROM CANCION c JOIN ARTISTA a ON c.ID_ARTISTA = a.ID_ARTISTA;
+
+SELECT c.TITULO , c.FECHA_LANZAMIENTO, a.NOMBRE FROM CANCION c JOIN ARTISTA a ON c.ID_ARTISTA = a.ID_ARTISTA;
